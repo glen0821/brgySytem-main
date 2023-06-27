1ed0293b1b6734a4fa0a08f8084713f099548516
glen0821 <domiojrglen@gmail.com>
2023-06-27 20:39:04 +0800

    NEW UPDATES

--- src/routes/ComponentsLib/LiveComponents/BarangayCertificate.svelte
+++ src/routes/ComponentsLib/LiveComponents/BarangayCertificate.svelte
@@ -232,11 +232,6 @@
                     on:click={()=>{editValueHandler(i)}}
                     >Edit</button>
 
-                    <button class="bg-red-500 font-bold text-white w-full p-2 hover:bg-red-600 border-b-2 border-white"
-                    on:click={()=>{editValueHandler(i)}}
-                    >Print</button>
-
-                    
                 </div>
 
                 {#if $compareCertValue === i}
